2ce2ce8fdec52bc15f21a32325b984c407c8537d
YA <YA-androidapp@users.noreply.github.com>
2022-04-16 13:46:09 +0900

    Create power-platform-basis.ps1

---
+++ power-platform-basis.ps1
@@ -0,0 +1,3 @@
+Set-ItemProperty C:\Users\Y\Documents\GitHub\power-platform-basis\ LastWriteTime $(Get-Date)
+Set-ItemProperty C:\Users\Y\Documents\GitHub\power-platform-basis.ps1 LastWriteTime $(Get-Date)
+pause
