c69173175ebb50ca19e43c2041c8cb60feeb22b3
YA <9039417+yzkn@users.noreply.github.com>
2023-08-29 21:07:12 +0900

    Add an item

--- js/items.js
+++ js/items.js
@@ -2046,4 +2046,11 @@ const repos = [
         item_summary: 'Dataverse アクション（バインドされていないアクション）を Power Automate クラウドフローから呼び出して、レコードの共有／レコードの割り当てを行う',
         item_tags: ['solution', 'powerapps', 'powerautomate'],
     },
+    {
+        item_image: 'App01.png',
+        item_title: 'PowerPlatform-PowerApps-PowerAutomate-TileMap-JmaNowcast-CoordinateConversion',
+        item_label: 'JmaNowcast',
+        item_summary: '経緯度で指定された特定地点を含むタイル地図画像を取得して Power Apps キャンバスアプリに表示する（経緯度からピクセル座標への座標変換）・数式を MathJax を利用して Web リソースに表示',
+        item_tags: ['solution', 'powerapps', 'powerautomate', 'webresource'],
+    },
 ];
