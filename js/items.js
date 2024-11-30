03acd1e0b17b0075cb53017554ee261161ab17a7
YA <9039417+yzkn@users.noreply.github.com>
2024-11-30 12:53:01 +0900

    Add an item

--- js/items.js
+++ js/items.js
@@ -2263,4 +2263,11 @@ const repos = [
         item_summary: 'Power Automate クラウドフローで FetchXML を使用して Dataverse テーブルのレコードを取得する',
         item_tags: ['solution', 'powerautomate'],
     },
+    {
+        item_image: 'Flow01.png',
+        item_title: 'PowerPlatform-PowerAutomate-DataverseWebAPI-ODataQuerySamples',
+        item_label: 'ODataQuerySamples',
+        item_summary: 'Power Automate クラウドフローで OData クエリを使用して Dataverse テーブルのレコードを取得する',
+        item_tags: ['solution', 'powerautomate'],
+    },
 ];
